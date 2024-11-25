c8a9cefd69a980592ea6fcbb9ae546c64aad7541
aniket <annadateaniket@gmail.com>
2024-11-25 17:56:54 +0530

    first commit . . .

---
+++ src/test/java/pack1/CreateOrder.java
@@ -0,0 +1,20 @@
+package pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		
+		
+
+	}
+
+}

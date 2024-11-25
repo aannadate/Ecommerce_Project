35c0531419cdf74207e6f9f3a1071c1c92b6e2cf
aniket <annadateaniket@gmail.com>
2024-11-25 18:39:08 +0530

    script : to fill the registration form

--- src/test/java/pack1/CreateOrder.java
+++ src/test/java/pack1/CreateOrder.java
@@ -1,5 +1,9 @@
 package pack1;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,16 +11,51 @@ public class CreateOrder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		WebDriver driver = new ChromeDriver();
-		
+		driver.manage().window().maximize();
+
 		String url = "https://demowebshop.tricentis.com/";
+
+		// invoke the url
 		driver.get(url);
+
+		// open registration page
+		driver.findElement(By.linkText("Register")).click();
+
+		// select the gender
+		driver.findElement(By.id("gender-male")).click();
+
+		// fill firstname
+		driver.findElement(By.id("FirstName")).sendKeys("Aniket");
+
+		// fill lastname
+		driver.findElement(By.id("LastName")).sendKeys("Annadate");
+
+		// fill email id
+		driver.findElement(By.id("Email")).sendKeys("aniket" + random() + "@gmail.com");
 		
-		System.out.println(" website invoked ");
+		//fill password
+		driver.findElement(By.id("Password")).sendKeys("Pass@123");
+		
+		//fill confirm password
+		driver.findElement(By.id("ConfirmPassword")).sendKeys("Pass@123");
+		
+		//submit the form
+		driver.findElement(By.id("register-button")).submit();
 		
 		
 
+	}
+
+	private static String random() {
+		// TODO Auto-generated method stub
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyddmmss");
+		String formattedDateTime = currentDateTime.format(formatter);
+		System.out.println("Current Date and Time: " + formattedDateTime);
+		return formattedDateTime;
+		
 	}
 
 }
